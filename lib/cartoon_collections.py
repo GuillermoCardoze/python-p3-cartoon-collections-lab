a04870d5757be977583ea3117d051d6bdf80275a
Guillermo <vital_tmv@yahoo.com>
2024-09-17 15:04:26 -0500

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(name):
+    for index, item in enumerate(name, start=1):
+        print(f"{index}. {item}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for word in calls:
+        if len(word) > 4:
+            return True
+    return False
+    
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in snacks:
+        if ingredient in cheeses:
+            return ingredient
+    return None
+    
